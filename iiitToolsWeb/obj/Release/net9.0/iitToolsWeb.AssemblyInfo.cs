1cb7ffcca59229728b45e2aeb34f4d75ff693a76
Golden-Jiang <gldenjiang0930@gmail.com>
2025-06-01 17:17:18 +0800

    2025/06/01-3 Log 改用依賴注入

--- iiitToolsWeb/obj/Release/net9.0/iitToolsWeb.AssemblyInfo.cs
+++ iiitToolsWeb/obj/Release/net9.0/iitToolsWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("iitToolsWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0268a179fd049feb23246543e1e26f67ccb047e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bda48ba125d8b40315bb92fabe9738bfdac155ed")]
 [assembly: System.Reflection.AssemblyProductAttribute("iitToolsWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("iitToolsWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
